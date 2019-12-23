6ab3fa916ae4babfa9374df1ca6b54c3a6bd3af0
Jorge Araújo <jdcmaraujo@gmail.com>
2019-12-23 15:33:01 +0000

    add marshmallow

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from views import translation_page
+from views import translation_page, translation_api
 import os
 
 def create_app():
@@ -11,12 +11,16 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     from database import db
     db.init_app(app)
+    from marsh_mallow import ma
+    ma.init_app(app)
     manager = Manager(app)
     manager.add_command('db', MigrateCommand)
     migrate = Migrate(app, db)
     app.register_blueprint(translation_page, url_prefix='')
+    app.register_blueprint(translation_api, url_prefix='/')
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     app.run()
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from sqlalchemy.dialects.postgresql import JSON
 from database import db
+from flask import Flask
+from marsh_mallow import ma
 
 class Translation(db.Model):
     __tablename__='translations'
@@ -7,7 +9,7 @@ class Translation(db.Model):
     uid = db.Column(db.String(),primary_key=True)
     state = db.Column(db.String())
     text_to_translate = db.Column(db.String())
-    translation = db.Column(db.String())
+    translation = db.Column(db.String(),default='...')
 
     def __init__(self,text_to_translate, state, uid):
         self.text_to_translate = text_to_translate
@@ -16,4 +18,19 @@ class Translation(db.Model):
     
     def __repr__(self):
         return '<id {}, text to translate{}>'.format(self.uid, self.text_to_translate)
+    
+    def __to_dict__(self):
+        return {
+            'uid' : self.uid,
+            'state' : self.state,
+            'text_to_translate' : self.text_to_translate,
+            'translation': self.translation
+        }
 
+
+class TranslationSchema(ma.Schema):
+    class Meta:
+        fields=("uid","state","text_to_translate", "translation")
+
+translation_schema = TranslationSchema()
+translations_schema = TranslationSchema(many=True)

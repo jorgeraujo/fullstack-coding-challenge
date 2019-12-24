fc3d9a8549e9661b21e7fd3734cc8420793233a6
Jorge Araújo <jdcmaraujo@gmail.com>
2019-12-24 17:35:26 +0000

    add payload validation

--- challenge/translation_api/routes.py
+++ challenge/translation_api/routes.py
@@ -1,19 +1,37 @@
 from models import Translation,translations_schema
 from flask import request, jsonify
 from . import translation_api
+from marshmallow import Schema, fields, ValidationError
 from challenge import db, socketio
+
+class ApiResponseSchema(Schema):
+    order_number = fields.Float()
+    price = fields.Float()
+    source_language = fields.Str()
+    status = fields.Str()
+    target_language = fields.Str()
+    text = fields.Str()
+    text_format = fields.Str() 
+    uid = fields.Str()
+    translated_text = fields.Str()
+
 
 @translation_api.route('/translations/', methods=['GET','POST'])
 def translate():
     if request.method == 'GET':
-        sql = 'select * from translations  order by length(translation) desc;'
+        sql = 'select * from translations order by length(translation) desc;'
         result = db.engine.execute(sql);
         data_ =  {'translations': translations_schema.dump(result)}
         return jsonify(data_)
     # endpoint for the translation callback 
     if request.method == 'POST':
         data = request.form.to_dict()
-        print(data)
+        # payload validation
+        try:
+            validated = ApiResponseSchema().load(data)
+        except ValidationError as err:
+            return 'Bad Request', 400
+
         try:
             translation_to_update = Translation.query.filter_by(uid=data['uid']).first()
             translation_to_update.state = data['status']
--- challenge/translation_page/routes.py
+++ challenge/translation_page/routes.py
@@ -3,6 +3,7 @@ from . import translation_page
 from models import Translation
 from challenge import db, uapi
 from marshmallow import Schema, fields
+from config import TRANSLATION_CALLBACK
 import json 
 
 # schema for the post request 
@@ -27,7 +28,7 @@ def index():
         # try to call unbabel api and add to the db 
         try:
              # call the unbabel API
-            result = uapi.post_translations(text=request_data['text_to_translate'],source_language='en', target_language='es',callback_url='http://9e17e987.ngrok.io/translations/')
+            result = uapi.post_translations(text=request_data['text_to_translate'],source_language='en', target_language='es',callback_url=TRANSLATION_CALLBACK)
             
             new_translation = Translation(text_to_translate=request_data['text_to_translate'],state=result.status,uid=result.uid)
             db.session.add(new_translation)

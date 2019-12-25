a8c903d3f6dacf6bdcc84098f35b9236e05818c2
Jorge Araújo <jdcmaraujo@gmail.com>
2019-12-25 18:11:53 +0000

    add cors to all routes

--- challenge/main.py
+++ challenge/main.py
@@ -1,7 +1,9 @@
 from challenge import create_app, socketio
 import eventlet
+from flask_cors import CORS
 eventlet.monkey_patch()
 app = create_app()
+CORS(app)
 
 if __name__ == '__main__':
     socketio.run(app)
